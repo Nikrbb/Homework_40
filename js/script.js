9b0f4882dfdb99b23cb9f727b7cf8e7b4dc61435
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2022-02-23 15:48:02 +0200

    update
    added prototype to constructor

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 (()=> {
     "use strict"
-
-    function Student(name, surname, birthYear) {
-        this.name = name;
-        this.surname = surname;
-        this.birthYear = birthYear;
-        this.age = () => new Date().getFullYear() - this.birthYear;
-        this.attendance = Array(10);
-        this.academicPerformance = Array(10);
-        this.present = () => {
+    const school = {
+        present() {
             for (let i = 0; i < this.attendance.length; i++) {
                 if (this.attendance[this.attendance.length - 1] !== undefined) {
                     throw new Error(`Attendance can't contain more then 10 visits`);
@@ -18,8 +11,8 @@
                     return;
                 }
             }
-        };
-        this.absent = () => {
+        },
+        absent() {
             for (let i = 0; i < this.attendance.length; i++) {
                 if (this.attendance[this.attendance.length - 1] !== undefined) {
                     throw new Error(`Attendance can't contain more then 10 visits`);
@@ -29,8 +22,8 @@
                     return;
                 }
             }
-        };
-        this.mark = grade => {
+        },
+        mark(grade) {
             for (let i = 0; i < this.academicPerformance.length; i++) {
                 if (this.academicPerformance[this.academicPerformance.length - 1] !== undefined) {
                     throw new Error(`Academic performance can't contain more then 10 marks`);
@@ -40,8 +33,8 @@
                     return;
                 }
             }
-        };
-        this.summary = () => {
+        },
+        summary() {
             const { averageAttendance } = this.attendance.reduce((average, current, index) => {
                 if (current) average.visits += 1;
                 average.amount = index + 1;
@@ -52,29 +45,37 @@
                 amount: null,
                 averageAttendance: null,
             });
-
             const { averagePerformance } = this.academicPerformance.reduce((average, current, index) => {
-                    average.marks += current;
-                    average.amount = index + 1;
-                    average.averagePerformance = average.marks / average.amount;
-                    return average;
+                average.marks += current;
+                average.amount = index + 1;
+                average.averagePerformance = average.marks / average.amount;
+                return average;
             }, {
                 marks: null,
                 amount: null,
                 averagePerformance: null,
             })
-
             if (averagePerformance > 9 && averageAttendance > 0.9) return "Ути какой молодчинка!";
             if (averagePerformance >= 9 || averageAttendance >= 0.9) return "Норм, но можно лучше";
             if (averagePerformance < 9 && averageAttendance < 0.9) return "Редиска!";
-
-
-        };
+        },
+        age() {
+         return   new Date().getFullYear() - this.birthYear;
+        },
     }
 
+    function Student(name, surname, birthYear) {
+        this.name = name;
+        this.surname = surname;
+        this.birthYear = birthYear;
+        this.attendance = Array(10);
+        this.academicPerformance = Array(10);
+    }
 
+    Student.prototype = school;
 
     let mark = new Student("Mark", "Bedarov", 2001);
+
     mark.present()
     mark.absent()
     mark.present()
@@ -84,6 +85,7 @@
     mark.mark(10)
     mark.mark(6)
     mark.mark(7)
+    console.log(mark.age())
     console.log(mark.name, mark.summary())
 
     let oleh = new Student("Oleh", "Tarasenko", 1998);
@@ -102,6 +104,7 @@
     oleh.mark(9)
     oleh.mark(10)
     oleh.mark(10)
+    console.log(oleh.age())
     console.log(oleh.name, oleh.summary())
 
 
@@ -114,5 +117,6 @@
     vasya.mark(11)
     vasya.mark(12)
     vasya.mark(10)
+    console.log(vasya.age())
     console.log(vasya.name, vasya.summary())
 })()
